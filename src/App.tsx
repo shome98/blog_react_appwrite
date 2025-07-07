8e26c674d4f196c4949db69a5d4d85eb624a6da3
Sowparna Shome <soupornoshome@gmail.com>
2025-07-08 01:14:33 +0530

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-3xl text-center'>hi from react</h1>
+      <h1 className="text-3xl text-center">hi from react</h1>
+      <div className="w-40 mx-auto flex flex-row items-center"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
