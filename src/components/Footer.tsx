044f38aa5d536acead4ee26ad575fbfecad972dc
Shome234 <shomeshome234@gmail.com>
2025-07-09 18:47:42 +0530

    footer create

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <section className="relative overflow-hidden py-5 bg-gray-700">
+                <h1 className="text-center text-2xl">this is footer</h1>
+            </section>
+        </>
+    )
+}
